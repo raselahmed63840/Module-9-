5e6b69dc3c23d54d6fb7c25b6fc260a9192de9ad
rasel <raselahmed63840@gmail.com>
2025-06-14 21:31:35 +0600

    4th

---
+++ 2nd Live class/readme.md
@@ -0,0 +1,9 @@
+# 1. API and how it combines backend and frontend 
+
+# 2. API call using fetch API (Post, Delete, Get, update)
+
+# 3. Task Manager Project using fetch API 
+
+
+
+
---
+++ 2nd Live class/script.js
@@ -0,0 +1,5 @@
+const API_URL = ' https://684a4ff5165d05c5d358484b.mockapi.io/task'; // Replace with your API URL
+
+async function fetchTasks() {
+  // TODO: implement fetching tasks
+}

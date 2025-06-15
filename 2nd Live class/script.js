f69a68c723fc5ba8c01173cf5ae7c4e59361dc50
rasel <raselahmed63840@gmail.com>
2025-06-15 18:28:29 +0600

    2nd

--- 2nd Live class/script.js
+++ 2nd Live class/script.js
@@ -1,5 +1,53 @@
 const API_URL = ' https://684a4ff5165d05c5d358484b.mockapi.io/task'; // Replace with your API URL
 
-async function fetchTasks() {
-  // TODO: implement fetching tasks
+
+const taskList = document.getElementById('task-list');
+ const fetchTasks = () =>{
+   const xhr = new XMLHttpRequest();
+ 
+   xhr.open('GET', API_URL, true);
+    xhr.onload = function() {
+      console.log(xhr);
+      if (xhr.status === 200) {
+        const tasks = JSON.parse(xhr.responseText);
+        console.log(tasks);
+        const taskList = document.getElementById('task-list');
+        taskList.innerHTML = ''; // Clear existing tasks
+
+        tasks.forEach(task => {
+          const li = document.createElement('li');
+          li.textContent = `${task.id}: ${task.title}`;
+          taskList.appendChild(li);
+        });
+      }
+       
+  
+  };
+  xhr.send();
+ };
+
+ const renderTask = (tasks) => {
+     taskList.innerHTML = ''; // Clear existing tasks
+     tasks.forEach((task) => {
+      //  console.log(task);
+      let li = document.createElement('li');
+      li.innerHTML = `
+       <div class="single-task">
+          <p>${task.id}.${task.title}</p>
+
+       
+         <button>Edit</button>
+         <button>Delete</button>
+       
+       
+       </div>
+       
+         
+      `;
+
+      taskList.appendChild(li);
+
+  });
 }
+
+ fetchTasks();
